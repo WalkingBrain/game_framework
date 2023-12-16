9434156002b28fa70ab33a5737c71f2aab4dce0e
WalkingBrain <123570875+WalkingBrain@users.noreply.github.com>
2023-12-16 10:46:19 +0100

    Generalization of function 'ask_weapon', now a method of class 'Player'

--- game.py
+++ game.py
@@ -73,27 +73,27 @@ class Player(Enemy):
         print("Inventory:")
         iterations = 0
         # Loop through the inventory
-        for item in player.inventory:
+        for item in self.inventory:
             print(item)
             iterations += 1
         # Check if the inventory is empty
         if iterations == 0: print("Inventory is empty")        
 
-def ask_weapon(user, target):
-    user.list_inventory()
-    print("Type 'exit' to exit")
-    choice = input("Enter the weapon you want to use: ")
-    if choice.lower() == "exit": # Handles exit logic
-        exit()
-    elif choice in user.inventory:
-        weapon = player_weapons[choice]
-        weapon.attack_weapon(target, user)
-    else:
-        print("You don't have that item")
-        ask_weapon()
+    def ask_weapon(self, target):
+        self.list_inventory()
+        print("Type 'exit' to exit")
+        choice = input("Enter the weapon you want to use: ")
+        if choice.lower() == "exit": # Handles exit logic
+            exit()
+        elif choice in self.inventory:
+            weapon = player_weapons[choice]
+            weapon.attack_weapon(target, self)
+        else:
+            print("You don't have that item")
+            self.ask_weapon()
 
 
-player = Player("player", 100, 10, 10) # Basic player
+
 player_weapons = {
     
 } # Dictionary of weapons, required for the code to work (Also hardcoded, but changeable if needed)
